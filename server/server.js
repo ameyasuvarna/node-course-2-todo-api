1975dcb2684a24f74a75aedad789632c78fd91d4
ameyadeloitte <asuvarna@deloitte.com>
2018-02-17 21:20:54 -0600

    Setup app for heroku

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ var {User} = require('./models/user');
 var {ObjectID} = require('mongodb');
 
 var app = express();
+const port = process.env.PORT || 3000
 
 app.use(bodyParser.json());
 
@@ -41,7 +42,7 @@ app.get('/todos/:id', (req, res) => {
     if (!todo) {
       return res.status(404).send();
     }
-    
+
     res.send({todo});
   }, (e) => {
     res.status(400).send(e);
@@ -50,8 +51,8 @@ app.get('/todos/:id', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Started on port 3000');
+app.listen(port, () => {
+  console.log(`Started up port ${port}`);
 });
 
 
