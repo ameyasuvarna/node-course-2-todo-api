fae363a6e123b465d690bf53503169803c9563d0
ameyadeloitte <asuvarna@deloitte.com>
2018-02-17 21:25:24 -0600

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 const REMOTE_MONGO = 'mongodb://admin:General123*@ds125198.mlab.com:25198/udemy-todoapi';
 const LOCAL_MONGO = 'mongodb://localhost:27017/TodoApp';
-const MONGO_URL = process.env.PORT ? REMOTE_MONGO : LOCAL_MONGO;
+const MONGO_URI = process.env.PORT ? REMOTE_MONGO : LOCAL_MONGO;
 
 mongoose.Promise = global.Promise;
 mongoose.connect(MONGO_URI);
