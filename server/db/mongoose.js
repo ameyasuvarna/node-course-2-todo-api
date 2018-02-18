cf9be795634dc8b612c0f632c559ea129274c34f
ameyadeloitte <asuvarna@deloitte.com>
2018-02-18 14:40:21 -0600

    Setup separate test and development env

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,11 +1,12 @@
 var mongoose = require('mongoose');
 
-const REMOTE_MONGO = 'mongodb://admin:General123*@ds125198.mlab.com:25198/udemy-todoapi';
-const LOCAL_MONGO = 'mongodb://localhost:27017/TodoApp';
-const MONGO_URI = process.env.PORT ? REMOTE_MONGO : LOCAL_MONGO;
+// const REMOTE_MONGO = 'mongodb://admin:General123*@ds125198.mlab.com:25198/udemy-todoapi';
+// const LOCAL_MONGO = 'mongodb://localhost:27017/TodoApp';
+// const MONGO_URI = process.env.PORT ? REMOTE_MONGO : LOCAL_MONGO;
+// mongoose.connect(MONGO_URI);
 
 mongoose.Promise = global.Promise;
-mongoose.connect(MONGO_URI);
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {
   mongoose
