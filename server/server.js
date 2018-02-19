fa94c7c7310b783453151b5f42c94d05c95fc7fe
ameyadeloitte <asuvarna@deloitte.com>
2018-02-19 00:07:43 -0600

    Add POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -97,9 +97,25 @@ app.patch('/todos/:id', (req, res) => {
   });
 });
 
+//POST /users
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+    // res.send(user);
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  });
+});
+
+app.post('/users')
+
 app.listen(port, () => {
   console.log(`Started up port ${port}`);
 });
-
 
 module.exports = {app};
